fa2c9f86d9638a18b29116c62712cd759b23799d
Aizirek Abdijalilova <aaizirek499@gmail.com>
2025-03-10 17:22:17 +0600

    ending Exam

--- src/components/pages/HomeSections.tsx
+++ src/components/pages/HomeSections.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Welcome from "./homeSections/Welcome";
+<<<<<<< HEAD
 import Exam from "./homeSections/Exam";
 
 const HomeSections = () => {
@@ -9,6 +10,17 @@ const HomeSections = () => {
             <Exam/>
         </div>
     );
+=======
+import About from "./homeSections/About";
+
+const HomeSections = () => {
+  return (
+    <div>
+      <Welcome />
+      <About />
+    </div>
+  );
+>>>>>>> 6d6f346296e422131da05900e6862c2d64f25c86
 };
 
 export default HomeSections;
